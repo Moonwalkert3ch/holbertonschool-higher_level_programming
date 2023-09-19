22651b2ddc1928e7267ce0189d738049a9ade09d
Moonwalkert3ch <6620@holbertonstudents.com>
2023-09-18 18:08:54 -0700

    fix doc style error

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,7 +2,11 @@
 
 """ python3 -c 'print(__import__("0-square").MyClass.__doc__)' """
 
+
 class Square:
-    """ Defines a square """
+
+    """ Defines a square  """
+
     pass
-""" empty class """
+
+    """ empty class """
