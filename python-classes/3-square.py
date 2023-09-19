123a106079ad41853187d39d70547ae5b6aa0d24
Moonwalkert3ch <6620@holbertonstudents.com>
2023-09-18 20:37:31 -0700

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" python3 -c 'print(__import__("1-Square.py").__doc__)' """
+"""python3 -c 'print(__import__("2-Square.py").MyClass.my_function.__doc__)'"""
+
+
+class Square:
+    """ Class that defines a square """
+
+    def __init__(self, size=0):
+        """
+        initialized Square class
+        param arg: size - defines size of square
+        raises exceptions:
+        for TypeError if not an integer
+        and ValueError if less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """"
+        calculates area of square
+        returns the current square area
+        """
+
+        return (self. __size ** 2)
