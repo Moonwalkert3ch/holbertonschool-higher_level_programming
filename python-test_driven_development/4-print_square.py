a59c0b76b714af5b60b8b12085c52699d7dcde71
Moonwalkert3ch <6620@holbertonstudents.com>
2023-09-20 17:03:36 -0700

    4-print_square.py

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("4-print_square.py").__doc__)"""
+
+
+def print_square(size):
+    """Returns: prints a square with the character #
+    Param Arg: size(int) - the size length of the square
+    Raise Exceptions:
+    TypeError if is not an a integer
+    ValueError if is lt 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    
+    for row in range(size):
+        [print("#", end="") for col in range(size)]
+        print("")
