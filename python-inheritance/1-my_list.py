b0b632377bae5e3dc91d86c3f13ce5d189616253
Moonwalkert3ch <6620@holbertonstudents.com>
2023-09-25 15:04:46 -0700

    1-my_list.py

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defines a function that prints the list, but sorted (ascending sort) """
+
+
+class MyList(list):
+    """class that inherits the list """
+
+def print_sorted(self):
+    """prints the list, but sorted (ascending sort) """
+    print(sorted(self))
