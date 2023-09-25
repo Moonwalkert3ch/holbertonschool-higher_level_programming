eabda72edd37a7cfb68454841e05b06c912ef7e6
Moonwalkert3ch <6620@holbertonstudents.com>
2023-09-25 15:38:53 -0700

    m

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a base geometry class """
+
+
+class BaseGeometry:
+    """Represents base geometry """
+
+    def area(self):
+        """Not implemented
+        Raises: Exception message due to no implementation"""
+        raise Exception("area() is not implemented")
