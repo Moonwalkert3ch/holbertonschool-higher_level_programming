ef56df1ffdafd7810a8f0c84a2d01f0d8d4d757b
Moonwalkert3ch <6620@holbertonstudents.com>
2023-09-25 15:24:01 -0700

    3-is_kind_of_class.py

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-Define a function that returns True if the object is an instance of, or if the object
-is an instance of a class that inherited from, the specified class; otherwise False
+Define a function that returns True if the object is an instance of,
+or if the object is an instance of a class that inherited from, the
+specified class; otherwise False
 """
 
 
