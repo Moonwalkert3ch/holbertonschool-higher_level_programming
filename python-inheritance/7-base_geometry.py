b717afd1a931443b7af17de262f04fbe818a39f2
Moonwalkert3ch <6620@holbertonstudents.com>
2023-09-25 15:57:15 -0700

    fix whitespace

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -9,6 +9,7 @@ class BaseGeometry:
         """Not implemented yet
         Raises: Exception message due to no implementation"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """
         Validates value as an integer
