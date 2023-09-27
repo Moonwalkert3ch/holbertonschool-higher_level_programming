03fccb67567c1e65bc5fc5ce9e96a7968789c4e7
Moonwalkert3ch <6620@holbertonstudents.com>
2023-09-26 20:11:50 -0700

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """Defines a script that adds all arguments to a python list and saves
 them to a file"""
-
-
 import sys
-
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
