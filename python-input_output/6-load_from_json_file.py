fe96eb942a61f5fd2c1c4dad6587e09fa3f3a445
Moonwalkert3ch <6620@holbertonstudents.com>
2023-09-26 19:49:20 -0700

    7-add_item.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 def load_from_json_file(filename):
     """creates an object
     Param Arg: filename - json file"""
-    with open(filename) as m:
-        return json.load(m)
+    with open(filename) as f:
+        return json.load(f)
