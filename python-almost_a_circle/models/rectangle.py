f6d3aa3ebdd9d59c3dea31acbc9c10e3d8789b13
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-02 14:56:34 -0700

    str

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -95,3 +95,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def __str__(self):
+        """Return the print() and str() representation"""
+        return 
