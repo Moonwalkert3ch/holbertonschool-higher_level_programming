b05f1d9c8b435ceb11ca0a481fc6a17be275f41e
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-02 16:17:53 -0700

    args

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -121,3 +121,20 @@ class Rectangle(Base):
         """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(type
                 (self).__id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args):
+        """
+         assigns an argument to each attribute
+         Param Arg: *args - no kw arg
+        """
+        if len(args) == 0:
+            return
+
+        try:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+        except IndexError:
+            pass
