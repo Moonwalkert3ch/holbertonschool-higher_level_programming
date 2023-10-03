28d825a8850895810bce1aa1c9c11eccceadf706
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-03 13:38:42 -0700

    dicts base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,3 +21,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries
+        Param Arg: list_dictionaries - list of dictionaries
+        """
+        if list_dictionaries is None of list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
