75246e20189881fc7b92d2031dfa5dc68f45f8d1
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-03 17:24:05 -0700

    test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,38 +6,37 @@
 """Unittests for base.py module"""
 
 
-class TestBase(unittest.TestCase):
-    def test_base_id(self):
-        """Test if Base assigns unique IDs"""
+class Test_Baseinit(unittest.TestCase):
+    """Test if Base assigns unique IDs"""
+    def test_uniq_id(self):
         base1 = Base()
         base2 = Base()
         self.assertEqual(base1.id, 1)
         self.assertEqual(base2.id, 2)
 
     def test_base_custom_id(self):
-            """Test if Base assigns a custom ID when provided"""
-            base1 = Base(10)
-            self.assertEqual(base1.id, 10)
+        """Test if Base assigns a custom ID when provided"""
+        base1 = Base(10)
+        self.assertEqual(base1.id, 10)
 
     def test_to_json_string(self):
-            """Test the to_json_string method"""
-            data = [{'key': 'value'}, {'key2': 'value2'}]
-            json_str = Base.to_json_string(data)
-            self.assertEqual(json_str, '[{"key": "value"}, {"key2": "value2"}]')
+        """Test the to_json_string method"""
+        data = [{'key': 'value'}, {'key2': 'value2'}]
+        json_str = Base.to_json_string(data)
+        self.assertEqual(json_str, '[{"key": "value"}, {"key2": "value2"}]')
 
     def test_from_json_string(self):
-            """Test the from_json_string method"""
-            json_str = '[{"key": "value"}, {"key2": "value2"}]'
-            data = Base.from_json_string(json_str)
-            self.assertEqual(data, [{'key': 'value'}, {'key2': 'value2'}])
+        """Test the from_json_string method"""
+        json_str = '[{"key": "value"}, {"key2": "value2"}]'
+        data = Base.from_json_string(json_str)
+        self.assertEqual(data, [{'key': 'value'}, {'key2': 'value2'}])
 
     def test_create(self):
-            """Test the create method"""
-            dummy_dict = {'id': 1, 'name': 'dummy'}
-            dummy = Base.create(**dummy_dict)
-            self.assertEqual(dummy.id, 1)
-            self.assertEqual(dummy.name, 'dummy')
-
+        """Test the create method"""
+        dummy_dict = {'id': 1, 'name': 'dummy'}
+        dummy = Base.create(**dummy_dict)
+        self.assertEqual(dummy.id, 1)
+        self.assertEqual(dummy.name, 'dummy')
 
 if __name__ == "__main__":
-    unittest.main()                                                                                                                                                                                                                                                                                                                                                                                                          
+    unittest.main()
