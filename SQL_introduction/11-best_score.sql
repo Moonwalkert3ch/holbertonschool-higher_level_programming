b79e72db4e33906b235da2ea6259266ae1beebfd
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-23 15:40:40 -0700

    list records gt 10

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- Lists all records with score >= 10 in second_table in my MySQL server.
-SELECT score, name FROM second_table WHERE score >= ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
