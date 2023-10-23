d3859c5dfb8abab97bcc8da91514eda92a3e56cd
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-23 16:56:17 -0700

    still have error

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,9 @@
 -- Creates db hbtn_0d_2 and new user
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+-- The user_0d_2 password should be set to user_0d_2_pwd
+-- If the database hbtn_0d_2 already exists, your script should not fail
+-- If the user user_0d_2 already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY user_0d_2_pwd;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED WITH mysql_native_password BY user_0d_2_pwd;
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
 FLUSH PRIVILEGES;
