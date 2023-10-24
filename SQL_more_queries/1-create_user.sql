c24bffc3cec35b0e0bf39e32bee94f33b2203e8f
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-23 18:44:33 -0700

    create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- Create server with all privileges on my server
+-- Create user with all privileges on my server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO USER 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
