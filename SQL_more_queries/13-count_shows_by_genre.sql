7068a8ea1e8283e2d51d53a8a3408de110b7e5ce
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-23 18:22:04 -0700

    fix wording

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Lists all genres and displays number of shows linked
-SELECT genres.name AS genre, COUNT(genres.id) AS number_of_shows
-FROM genres JOIN tv_show_genre
-ON tv_show_genre.genres_id = genres.id
+SELECT tv_genres.name AS genre, COUNT(tv_genres.id) AS number_of_shows
+FROM tv_genres JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
