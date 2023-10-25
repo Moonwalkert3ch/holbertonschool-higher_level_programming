1625e837ef217bc55b6067965125e04d02a11a92
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-24 18:06:38 -0700

    correct style error

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,8 +5,8 @@ import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306, 
-                        passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
+                         passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
 
     c.execute("SELECT * FROM states ORDER BY id")
