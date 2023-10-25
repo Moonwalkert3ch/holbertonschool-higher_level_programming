6f57f0d638be6643906e32360f498cb6b9807608
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-24 20:53:28 -0700

    syntax error

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -19,5 +19,5 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id):
+    for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
