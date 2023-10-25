956ee692c7ea5c206b1fe427e7b8282ecac4e3a9
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-24 21:52:26 -0700

    14-model_city_fetch_by_state.sql

--- python-object_relational_mapping/14-model_city_fetch_by_state.sql
+++ python-object_relational_mapping/14-model_city_fetch_by_state.sql
@@ -1,23 +1,21 @@
--- Create database hbtn_0e_14_usa, tables states and cities + some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_14_usa;
-USE hbtn_0e_14_usa;
+#!/usr/bin/python3
+"""adds the State object “Louisiana” to the database"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-CREATE TABLE IF NOT EXISTS states (
-	id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
-);
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
 
-CREATE TABLE IF NOT EXISTS cities (
-	id INT NOT NULL AUTO_INCREMENT,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY(state_id) REFERENCES states(id)
-);
-INSERT INTO cities (state_id, name) VALUES (1, "San Francisco"), (1, "San Jose"), (1, "Los Angeles"), (1, "Fremont"), (1, "Livermore");
-INSERT INTO cities (state_id, name) VALUES (2, "Page"), (2, "Phoenix");
-INSERT INTO cities (state_id, name) VALUES (3, "Dallas"), (3, "Houston"), (3, "Austin");
-INSERT INTO cities (state_id, name) VALUES (4, "New York");
-INSERT INTO cities (state_id, name) VALUES (5, "Las Vegas"), (5, "Reno"), (5, "Henderson"), (5, "Carson City");
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City, State).join(State).order_by(City.id).all()
+
+    for city, state in cities:
+	print("{}: ({:d}) {}".format(state.name, city.id, city.name))
+
+	session.close()
