074cd8842c0cf71de855c965d096b8405ec9444b
Moonwalkert3ch <6620@holbertonstudents.com>
2023-11-28 17:32:53 -0600

    adjust loop

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 
 const threeLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (const threeLines of threeLines) {
-  console.log(threeLines);
+for (const lines of threeLines) {
+  console.log(lines);
 }
