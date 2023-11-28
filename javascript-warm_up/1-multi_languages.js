1a6ba55dc249e79a7d50f6dfd15c96722bc6c5b0
Moonwalkert3ch <6620@holbertonstudents.com>
2023-11-28 16:57:00 -0600

    task 1

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const lineOne = 'C is fun';
+console.log(lineOne);
+const lineTwo = 'Python is cool';
+console.log(lineTwo);
+const lineThree = 'JavaScript is amazing';
+console.log(lineThree);
