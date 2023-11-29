beec7bd1eb03fa397c977f2f18965708745b64b8
Moonwalkert3ch <6620@holbertonstudents.com>
2023-11-28 18:27:13 -0600

    change variable name

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const args =  process.argv.slice(2);
-const arg_to_number = args.map(arg => parseInt(arg));
-const numbers = arg_to_number.filter(num => !isNaN(num));
+const args = process.argv.slice(2);
+const argToNumber = args.map(arg => parseInt(arg));
+const numbers = argToNumber.filter(num => !isNaN(num));
 
 if (numbers.length <= 1) {
   console.log(0);
 } else {
-  const sortedNum = numbers.sort((a, b) => b = a);
+  const sortedNum = numbers.sort((a, b) => b - a);
   console.log(sortedNum[1]);
 }
