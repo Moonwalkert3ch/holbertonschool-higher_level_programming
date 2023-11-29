aa08f44d0d393cedc5032adbbbca8a48d413901b
Moonwalkert3ch <6620@holbertonstudents.com>
2023-11-28 21:08:20 -0600

    constructor

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,20 @@
 #!/usr/bin/node
 class Rectangle {
-  if ((w > 0) && (h > 0)) {
-    this.width = w;
-    this.height = h;
-  }
-}
-
-print () {
-  for (let i = 0; i < this.height; i++) {
-    let rows = '';
-    for (let j = 0; j < this.width; j++) {
-      rows += 'X';
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
-    console.log(rows);
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rows = '';
+      for (let j = 0; j < this.width; j++) {
+        rows += 'X';
+      }
+      console.log(rows);
+    }
   }
 }
 
