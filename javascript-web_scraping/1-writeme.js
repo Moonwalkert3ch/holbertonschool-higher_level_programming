013ab5f9938370f22089b2fa87ecee030e8f70a8
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-03 12:09:02 -0600

    fix semistandard

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-function writeToFile(filePath, data) {
+function writeToFile (filePath, data) {
   fs.writeFile(filePath, data, 'utf8', (error) => {
     if (error) {
       console.error('An error has occurred');
