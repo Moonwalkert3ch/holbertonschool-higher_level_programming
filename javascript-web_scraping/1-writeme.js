bcff3b92787493ebf5a1ebf8a3b361fdf769bc49
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-03 12:00:52 -0600

    string to file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+function stringToFile (filePath, data) {
+  fs.stringToFile(filePath, data, 'utf8', (error) => {
+    if (error) {
+      console.error('An error has occurred');
+    }
+  });
+}
+stringToFile(filePath, content);
