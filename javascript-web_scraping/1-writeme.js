7610733aba2d2407af4a168cadf6cf4d7f6e0119
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-03 12:05:30 -0600

    rename func

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-function stringToFile (filePath, data) {
-  fs.stringToFile(filePath, data, 'utf8', (error) => {
+function stringToFile(filePath, data) {
+  fs.stringFile(filePath, data, 'utf8', (error) => {
     if (error) {
       console.error('An error has occurred');
     }
