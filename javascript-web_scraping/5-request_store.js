a06d785e4572c1f00f8595519c2c75dacb268838
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-03 19:47:13 -0600

    request and store

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const fileOutput = argv[3];
+
+request(url, function (err, body, response) {
+    if (err) {
+        console.error(`Error code: ${err}`);
+    } else if (response.statusCode === 200) {
+        fs.writeFile(fileOutput, body, 'utf8', (error) {
+            if (error) {
+                console.error(`An error has occurred`)
+            }
+        });
+    }
+});
