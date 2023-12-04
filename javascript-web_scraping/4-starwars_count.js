2708b7b4a7d2e7fab83e9d80d4eaf3a1f1758279
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-03 19:28:43 -0600

    fix semistandard

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,21 +4,21 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, function (error, response, body) {
-    if (error) {
-        console.error(`error code: ${response.statusCode}`);
-    } else if (response.statusCode === 200) {
-        const results = JSON.parse(body).results;
-        const count = results.reduce(function (occurrence, film) {
-            if (film.characters.find(function (character) {
-                return character.endsWith('/18/');
-            })) {
-                occurrence = occurrence + 1;
-            }
-            return occurrence;
-        }, 0);
+  if (error) {
+    console.error(`error code: ${response.statusCode}`);
+  } else if (response.statusCode === 200) {
+    const results = JSON.parse(body).results;
+    const count = results.reduce(function (occurrence, film) {
+      if (film.characters.find(function (character) {
+        return character.endsWith('/18/');
+      })) {
+        occurrence = occurrence + 1;
+      }
+      return occurrence;
+    }, 0);
 
-        console.log(count);
-    } else {
-        console.error('Error code:', response.statusCode);
-    }
+    console.log(count);
+  } else {
+    console.error('Error code:', response.statusCode);
+  }
 });
