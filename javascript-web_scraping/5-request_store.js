a27364e27ec46c8d83e70d54af3af42dd17a77c4
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-03 19:59:04 -0600

    the order of params is important body is after response

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,16 +2,19 @@
 
 const request = require('request');
 const fs = require('fs');
+
 const url = process.argv[2];
 const fileOutput = process.argv[3];
 
-request(url, function (err, body, response) {
-  if (err) {
+request(url, 'utf8', function (error, response, body) {
+  if (error) {
     console.error(`Error code: ${err}`);
   } else if (response.statusCode === 200) {
-    fs.writeFile(fileOutput, body, 'utf8', (error) => {
-      if (error) {
-        console.error('An error has occurred');
+    fs.writeFile(fileOutput, body, 'utf8', (err) => {
+      if (err) {
+        console.error('An error has occurred: ${err}');
+      } else {
+        console.log(`${fileOutput}`);
       }
     });
   }
