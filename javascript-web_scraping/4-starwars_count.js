bded437b95e1355fcda1941ef9d042c9f6572afb
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-03 19:27:33 -0600

    added the count increment

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,16 +3,22 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, (error, response, body) => {
+request.get(url, function (error, response, body) {
     if (error) {
-        console.error(`An error has occurred`);
-    } else if (response.statusCode === 200) {
-        const filmsResponse = JSON.parse(body);
-        const wedgeFilms = filmsResponse.results.filter(movie =>
-            movie.characters.includes('https://swapi-api.hbtn.io/api/people/18')
-        );
-        console.log(`${wedgeFilms.length}`);
-    } else {
         console.error(`error code: ${response.statusCode}`);
+    } else if (response.statusCode === 200) {
+        const results = JSON.parse(body).results;
+        const count = results.reduce(function (occurrence, film) {
+            if (film.characters.find(function (character) {
+                return character.endsWith('/18/');
+            })) {
+                occurrence = occurrence + 1;
+            }
+            return occurrence;
+        }, 0);
+
+        console.log(count);
+    } else {
+        console.error('Error code:', response.statusCode);
     }
 });
