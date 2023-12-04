0bf8180c93fef6a3af920a1a1cdf0c9776e9e091
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-03 19:51:46 -0600

    semistandard fix

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -3,16 +3,16 @@
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const fileOutput = argv[3];
+const fileOutput = process.argv[3];
 
 request(url, function (err, body, response) {
-    if (err) {
-        console.error(`Error code: ${err}`);
-    } else if (response.statusCode === 200) {
-        fs.writeFile(fileOutput, body, 'utf8', (error) => {
-            if (error) {
-                console.error(`An error has occurred`)
-            }
-        });
-    }
+  if (err) {
+    console.error(`Error code: ${err}`);
+  } else if (response.statusCode === 200) {
+    fs.writeFile(fileOutput, body, 'utf8', (error) => {
+      if (error) {
+        console.error('An error has occurred');
+      }
+    });
+  }
 });
