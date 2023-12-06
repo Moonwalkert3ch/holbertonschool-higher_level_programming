b423f174fa68167e51dd45241d7b9063969b4055
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-05 20:10:10 -0600

    add event listeners

---
+++ javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// add a click event listener to #red_header div
+$('#red_header').click(function () {
+  // update text to red font
+  $('header').css('color', '#FF0000');
+});
---
+++ javascript-web_jquery/3-script.js
@@ -0,0 +1,5 @@
+// Add a click event listener to the #red_header div
+$('#red_header').click(function () {
+  // Add the 'red' class to the <header>
+  $('header').addClass('red');
+});
