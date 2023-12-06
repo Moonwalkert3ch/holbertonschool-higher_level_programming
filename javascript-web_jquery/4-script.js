0de4f6b68d8ddd19d254ed3186b0d99e62e7f178
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-05 20:13:34 -0600

    toggle colors

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,5 @@
+// Add a click event listener to the #toggle_header div
+$('#toggle_header').click(function() {
+    // Toggle the 'red' and 'green' classes on the <header>
+    $('header').toggleClass('red green');
+  });
