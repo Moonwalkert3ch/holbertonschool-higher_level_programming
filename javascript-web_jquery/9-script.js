9a37b95e5b427f9c80deb57c1f4f9598706894b7
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-05 20:47:01 -0600

    semi

--- javascript-web_jquery/9-script.js
+++ javascript-web_jquery/9-script.js
@@ -1,10 +1,10 @@
 // Make an AJAX request to fetch translation
 $.ajax({
-    url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
-    method: 'GET',
-    dataType: 'json',
-    success: function(translation) {
-        // Update with the translation
-        $('#hello').text(translation.hello);
-    },
+  url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+  method: 'GET',
+  dataType: 'json',
+  success: function (translation) {
+    // Update with the translation
+    $('#hello').text(translation.hello);
+  }
 });
